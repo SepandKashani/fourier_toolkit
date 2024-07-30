494c045ffbe5989f1cb1377bfa283d61268efecd
Sepand KASHANI <sepand@kashani.ch>
2024-07-30 11:19:06 +0200

    [linalg] Add hadamard_outer2()

--- src/fourier_toolkit/linalg.py
+++ src/fourier_toolkit/linalg.py
@@ -3,6 +3,7 @@ import opt_einsum as oe
 
 __all__ = [
     "hadamard_outer",
+    "hadamard_outer2",
 ]
 
 
@@ -43,6 +44,58 @@ def hadamard_outer(x: np.ndarray, *args: list[np.ndarray]) -> np.ndarray:
     for d in range(D):
         outer_args[2 * d] = args[d]
         outer_args[2 * d + 1] = (d,)
+
+    y = oe.contract(  # (..., N1,...,ND)
+        *(x, x_ind),
+        *outer_args,
+        o_ind,
+        use_blas=True,
+        optimize="auto",
+    )
+    return y
+
+
+def hadamard_outer2(x: np.ndarray, *args: list[np.ndarray]) -> np.ndarray:
+    r"""
+    Compute Hadamard-reduction of `x` with (broadcasted) outer product of `args`.
+
+    In other words, this function computes the following:
+
+    .. math::
+
+       y = \sum_{p} x[p, a1,...,aD] * A1[p, a1] * ... * AD[p, aD]
+
+    Parameters
+    ----------
+    x: ndarray
+        (P, ..., N1,...,ND)
+    args[k]: ndarray
+        (P, Nk)
+
+    Returns
+    -------
+    y: ndarray
+        (..., N1,...,ND)
+
+    Note
+    ----
+    All inputs must share the same dtype precision.
+    """
+    D = len(args)
+    assert all(A.ndim == 2 for A in args)
+
+    P, sh = x.shape[0], tuple(A.shape[1] for A in args)
+    assert all(A.shape[0] == P for A in args)
+
+    assert x.ndim >= D + 1
+    assert x.shape[-D:] == sh
+
+    x_ind = (0, Ellipsis, *range(1, D + 1))
+    o_ind = (Ellipsis, *range(1, D + 1))
+    outer_args = [None] * (2 * D)
+    for d in range(D):
+        outer_args[2 * d] = args[d]
+        outer_args[2 * d + 1] = (0, d + 1)
 
     y = oe.contract(  # (..., N1,...,ND)
         *(x, x_ind),
