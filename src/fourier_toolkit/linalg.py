24bbd18160e9132426960994573d103eba7607a3
Sepand KASHANI <sepand@kashani.ch>
2024-08-04 13:59:22 +0200

    [linalg] Add hadamard_outer3()

--- src/fourier_toolkit/linalg.py
+++ src/fourier_toolkit/linalg.py
@@ -4,6 +4,7 @@ import opt_einsum as oe
 __all__ = [
     "hadamard_outer",
     "hadamard_outer2",
+    "hadamard_outer3",
 ]
 
 
@@ -63,7 +64,7 @@ def hadamard_outer2(x: np.ndarray, *args: list[np.ndarray]) -> np.ndarray:
 
     .. math::
 
-       y = \sum_{p} x[p, a1,...,aD] * A1[p, a1] * ... * AD[p, aD]
+       y[a1,...,aD] = \sum_{p} x[p, a1,...,aD] * A1[p, a1] * ... * AD[p, aD]
 
     Parameters
     ----------
@@ -98,6 +99,58 @@ def hadamard_outer2(x: np.ndarray, *args: list[np.ndarray]) -> np.ndarray:
         outer_args[2 * d + 1] = (0, d + 1)
 
     y = oe.contract(  # (..., N1,...,ND)
+        *(x, x_ind),
+        *outer_args,
+        o_ind,
+        use_blas=True,
+        optimize="auto",
+    )
+    return y
+
+
+def hadamard_outer3(x: np.ndarray, *args: list[np.ndarray]) -> np.ndarray:
+    r"""
+    Compute Hadamard-expansion of `x` with (broadcasted) outer product of `args`.
+
+    In other words, this function computes the following:
+
+    .. math::
+
+       y[p, a1,...,aD] = x[a1,...,aD] * A1[p, a1] * ... * AD[p, aD]
+
+    Parameters
+    ----------
+    x: ndarray
+        (..., N1,...,ND)
+    args[k]: ndarray
+        (P, Nk)
+
+    Returns
+    -------
+    y: ndarray
+        (P, ..., N1,...,ND)
+
+    Note
+    ----
+    All inputs must share the same dtype precision.
+    """
+    D = len(args)
+    assert all(A.ndim == 2 for A in args)
+
+    P, sh = args[0].shape[0], tuple(A.shape[1] for A in args)
+    assert all(A.shape[0] == P for A in args)
+
+    assert x.ndim >= D
+    assert x.shape[-D:] == sh
+
+    x_ind = (Ellipsis, *range(1, D + 1))
+    o_ind = (0, Ellipsis, *range(1, D + 1))
+    outer_args = [None] * (2 * D)
+    for d in range(D):
+        outer_args[2 * d] = args[d]
+        outer_args[2 * d + 1] = (0, d + 1)
+
+    y = oe.contract(  # (P, ..., N1,...,ND)
         *(x, x_ind),
         *outer_args,
         o_ind,
--- src/fourier_toolkit_tests/test_linalg.py
+++ src/fourier_toolkit_tests/test_linalg.py
@@ -161,3 +161,89 @@ class TestHadamardOuter2:
 
         y = np.sum(x * A, axis=0)  # (..., N1,...,ND)
         return y.astype(dtype)
+
+
+class TestHadamardOuter3:
+    def test_value(self, x, args, y_gt):
+        # output value matches ground truth.
+        y = ftk_linalg.hadamard_outer3(x, *args)
+        assert ct.allclose(y, y_gt, y_gt.dtype)
+
+    def test_prec(self, x, args):
+        # output precision (not dtype!) matches input precision.
+        assert all(x.dtype == A.dtype for A in args)
+        y = ftk_linalg.hadamard_outer3(x, *args)
+        assert y.dtype == x.dtype
+
+    # Fixtures ----------------------------------------------------------------
+    @pytest.fixture(params=[1, 2, 3, 4])
+    def space_dim(self, request) -> int:
+        return request.param
+
+    @pytest.fixture(params=[1, 3])
+    def p_dim(self, request) -> int:
+        return request.param
+
+    @pytest.fixture
+    def space_shape(self, space_dim) -> tuple[int]:
+        rng = np.random.default_rng()
+        N = rng.integers(5, 11, space_dim)
+        return tuple(map(int, N))  # (N1,...,ND)
+
+    @pytest.fixture(
+        params=[
+            (),
+            (1,),
+            (5, 3, 4),
+        ]
+    )
+    def stack_shape(self, request) -> tuple[int]:
+        return request.param  # (...)
+
+    @pytest.fixture(
+        params=[
+            np.int32,
+            np.int64,
+            np.float32,
+            np.float64,
+            np.complex64,
+            np.complex128,
+        ]
+    )
+    def dtype(self, request) -> np.dtype:
+        return np.dtype(request.param)
+
+    @pytest.fixture
+    def x(self, space_shape, stack_shape, dtype) -> np.ndarray:
+        size = np.prod(stack_shape, dtype=int)
+        size *= np.prod(space_shape, dtype=int)
+        x = np.linspace(-5, 5, size)
+        return x.reshape(*stack_shape, *space_shape).astype(dtype)  # (*sh, N1,...,ND)
+
+    @pytest.fixture
+    def args(self, p_dim, space_dim, space_shape, dtype) -> list[np.ndarray]:
+        args = [None] * space_dim
+        for d in range(space_dim):
+            A = np.linspace(-40, 5, p_dim * space_shape[d])
+            A = A.reshape(p_dim, space_shape[d])
+            args[d] = A.astype(dtype)
+        return args
+
+    @pytest.fixture
+    def y_gt(self, x, args, dtype) -> np.ndarray:
+        D = len(args)
+        P = args[0].shape[0]
+        N = x.shape[-D:]  # (N1,...,ND)
+        Ns = len(x.shape[:-D])  # (...,)
+
+        A = [None] * D
+        for d, _A in enumerate(args):
+            sh = [1] * (1 + Ns + D)
+            sh[0] = P
+            sh[1 + Ns + d] = N[d]
+
+            A[d] = _A.reshape(*sh)
+        A = functools.reduce(np.multiply, A)  # (P, 1...1, N1,...,ND)
+
+        y = x * A  # (P, ..., N1,...,ND)
+        return y.astype(dtype)
