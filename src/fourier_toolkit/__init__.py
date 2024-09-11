63092667252da38712ed551551f180a219e24a99
Sepand KASHANI <sepand@kashani.ch>
2024-09-11 09:13:40 +0200

    [init] Expose NUFFT[12]() at top level.

--- src/fourier_toolkit/__init__.py
+++ src/fourier_toolkit/__init__.py
@@ -4,5 +4,7 @@ __version__ = importlib.metadata.version("fourier_toolkit")
 
 from .nu2nu import *
 from .nu2u import *
+from .nufft1 import *
+from .nufft2 import *
 from .u2nu import *
 from .u2u import *
