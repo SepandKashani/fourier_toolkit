c96e0904974beb55cb44daa2bdaf833f7d931d1d
Sepand KASHANI <sepand@kashani.ch>
2025-12-30 16:50:05 +0100

    [test] Add TestCastWarn()

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,7 @@
 import pytest
+import numpy as np
+from numpy.typing import DTypeLike, NDArray
+import warnings
 import fourier_toolkit.util as ftku
 
 
@@ -18,3 +21,53 @@ class TestBroadcastSeq:
 
     def test_casts(self):
         assert ftku.broadcast_seq(x=1.1, cast=int) == (1,)
+
+
+class TestCastWarn:
+    @staticmethod
+    def random_array(dtype: DTypeLike, seed: int = 0) -> NDArray:
+        rng = np.random.default_rng(seed)
+        x = rng.standard_normal(size=(3, 4))
+        return x.astype(dtype)
+
+    dtypes = pytest.mark.parametrize(
+        "dtype",
+        [
+            np.float32,
+            np.float64,
+            np.complex64,
+            np.complex128,
+            np.int32,
+            np.int64,
+            np.uint32,
+            np.uint64,
+        ],
+    )
+
+    @dtypes
+    def test_no_op(self, dtype):
+        dtype = np.dtype(dtype)
+        x = self.random_array(dtype)
+
+        # no warning must be emitted
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            y = ftku.cast_warn(x, dtype)
+            assert not w
+
+        assert y is x
+
+    @dtypes
+    def test_warns(self, dtype):
+        in_dtype = np.dtype(dtype)
+        out_dtype = np.dtype(np.float16)
+
+        x = self.random_array(in_dtype)
+
+        # warning must be emitted
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            y = ftku.cast_warn(x, out_dtype)
+            assert w
+
+        assert y.dtype == out_dtype
