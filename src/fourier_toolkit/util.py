e567f5f938c9b76bc6586fd41f8111811abb2494
Sepand KASHANI <sepand@kashani.ch>
2025-12-30 13:50:15 +0100

    [util] Add cast_warn()

--- src/fourier_toolkit/util.py
+++ src/fourier_toolkit/util.py
@@ -1,9 +1,13 @@
+import warnings
+
 from typing import Optional
 from collections.abc import Iterable, Callable
+from numpy.typing import NDArray, DTypeLike
 
 
 __all__ = [
     "broadcast_seq",
+    "cast_warn",
 ]
 
 
@@ -28,4 +32,17 @@ def broadcast_seq(
         assert len(y) == N
 
     y = tuple(map(cast, y))
+    return y
+
+
+def cast_warn(x: NDArray, dtype: DTypeLike) -> NDArray:
+    """
+    Cast `x` to `dtype` if type mis-match.
+
+    Emit warning when cast occurs.
+    """
+    y = x.astype(dtype, copy=False)
+    if x.dtype != y.dtype:
+        msg = f"{x.shape}: {x.dtype} -> {y.dtype} cast performed."
+        warnings.warn(msg)
     return y
