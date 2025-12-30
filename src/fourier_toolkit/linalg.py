e1e2104f53fb1d9c3efd63f9b4b696a79434e96a
Sepand KASHANI <sepand@kashani.ch>
2025-12-30 18:20:03 +0100

    [linalg] Add hadamard_outer()

---
+++ src/fourier_toolkit/linalg.py
@@ -0,0 +1,54 @@
+from numpy.typing import NDArray
+import opt_einsum as oe
+
+__all__ = [
+    "hadamard_outer",
+]
+
+
+def hadamard_outer(x: NDArray, *args: list[NDArray]) -> NDArray:
+    r"""
+    Compute Hadamard product of `x` with outer product of `args`:
+
+    .. math::
+
+       y = x \odot (A_{1} \otimes\cdots\otimes A_{D})
+
+    Parameters
+    ----------
+    x: NDArray
+        (..., N1,...,ND)
+    args[k]: NDArray
+        (Nk,)
+
+    Returns
+    -------
+    y: NDArray
+        (..., N1,...,ND)
+
+    Note
+    ----
+    All inputs must share the same dtype precision.
+    """
+    D = len(args)
+    assert all(A.ndim == 1 for A in args)
+    sh = tuple(A.size for A in args)
+
+    assert x.ndim >= D
+    assert x.shape[-D:] == sh
+
+    x_ind = (Ellipsis, *range(D))
+    o_ind = (Ellipsis, *range(D))
+    outer_args = [None] * (2 * D)
+    for d in range(D):
+        outer_args[2 * d] = args[d]
+        outer_args[2 * d + 1] = (d,)
+
+    y = oe.contract(  # (..., N1,...,ND)
+        *(x, x_ind),
+        *outer_args,
+        o_ind,
+        use_blas=True,
+        optimize="auto",
+    )
+    return y
