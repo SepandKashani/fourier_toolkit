3d76e48b99f15a557b4d019aa8d29dae2ac7e675
Sepand KASHANI <sepand@kashani.ch>
2025-12-30 16:46:43 +0100

    [util] Add UniformSpec()

--- src/fourier_toolkit/util.py
+++ src/fourier_toolkit/util.py
@@ -1,7 +1,7 @@
 import warnings
-
+from dataclasses import dataclass
 from typing import Optional
-from collections.abc import Iterable, Callable
+from collections.abc import Iterable, Callable, Iterator
 from numpy.typing import NDArray, DTypeLike
 import numpy as np
 
@@ -9,6 +9,7 @@ __all__ = [
     "broadcast_seq",
     "cast_warn",
     "TranslateDType",
+    "UniformSpec",
 ]
 
 
@@ -84,3 +85,129 @@ class TranslateDType:
 
     def to_complex(self) -> np.dtype:
         return self.map_from_float[(self._fdtype, "c")]
+
+
+@dataclass
+class UniformSpec:
+    r"""
+    Multi-dimensional uniform mesh specifier.
+
+    Defines points :math:`\bbx_{m} \in \bR^{D}` where each point lies on the
+    regular lattice
+
+    .. math::
+
+       \bbx_{\bbm} = \bbx_{0} + \Delta_{\bbx} \odot \bbm,
+       \qquad
+       [\bbm]_{d} \in \{0,\ldots,M_{d}-1\}
+
+    """
+
+    start: tuple[float]
+    step: tuple[float]
+    num: tuple[int]
+
+    def __init__(
+        self,
+        start=None,
+        step=None,
+        num=None,
+        *,
+        center=None,
+        span=None,
+    ):
+        r"""
+        Initialize mesh spec from (start,step,num) or (center,span,num).
+
+        Parameters (option 1)
+        ---------------------
+        start: tuple[float]
+            \bbx_{0} \in \bR^{D}
+        step: tuple[float]
+            \Delta_{\bbx} \in \bR_{+}^{D}
+        num: tuple[int]
+            (M1,...,MD) lattice size
+
+        Parameters (option 2)
+        ---------------------
+        center: tuple[float]
+            (D,) geometric center
+        span: tuple[float]
+            (D,) axial widths
+        num: tuple[int]
+            (M1,...,MD) lattice size
+
+        Scalars are broadcast to all dimensions.
+        """
+
+        provided = lambda _: _ is not None  # noqa: E731
+        if all(map(provided, (start, step, num))):
+            start = broadcast_seq(start, None, float)
+            step = broadcast_seq(step, None, float)
+            assert all(s > 0 for s in step)
+            num = broadcast_seq(num, None, int)
+            assert all(n > 0 for n in num)
+
+            D = max(map(len, (start, step, num)))
+            self.start = broadcast_seq(start, D)
+            self.step = broadcast_seq(step, D)
+            self.num = broadcast_seq(num, D)
+        elif all(map(provided, (center, span, num))):
+            center = broadcast_seq(center, None, float)
+            span = broadcast_seq(span, None, float)
+            assert all(s > 0 for s in span)
+            num = broadcast_seq(num, None, int)
+            assert all(n > 0 for n in num)
+
+            D = max(map(len, (center, span, num)))
+            center = broadcast_seq(center, D)
+            span = broadcast_seq(span, D)
+            self.num = broadcast_seq(num, D)
+            self.start = tuple(c - 0.5 * s for (c, s) in zip(center, span))
+            self.step = tuple(s / (n - 1) for (s, n) in zip(span, self.num))
+        else:
+            raise ValueError
+
+    @property
+    def ndim(self) -> int:
+        return len(self.start)
+
+    def __iter__(self) -> Iterator:
+        for d in range(self.ndim):
+            yield (self.start[d], self.step[d], self.num[d])
+
+    @property
+    def span(self) -> tuple[float]:
+        return tuple(dx * (nx - 1) for (_, dx, nx) in self)
+
+    @property
+    def center(self) -> tuple[float]:
+        return tuple(x0 + 0.5 * dx * (nx - 1) for (x0, dx, nx) in self)
+
+    def __neg__(self) -> "UniformSpec":
+        """
+        Negate all mesh coordinates.
+        """
+        neg_x0 = tuple(-(x0 + dx * (nx - 1)) for (x0, dx, nx) in self)
+        return UniformSpec(start=neg_x0, step=self.step, num=self.num)
+
+    def meshgrid(self, xp, sparse: bool = False) -> tuple[NDArray]:
+        """
+        Returns
+        -------
+        mesh: tuple[NDArray]
+            (D,) axial knot coordinates
+        """
+        mesh_1D = tuple(x0 + dx * xp.arange(nx) for (x0, dx, nx) in self)
+        mesh = xp.meshgrid(*mesh_1D, indexing="ij", sparse=sparse)
+        return mesh
+
+    def knots(self, xp) -> NDArray:
+        """
+        Returns
+        -------
+        x: NDArray
+            (M1,...,MD,D) mesh coordinates
+        """
+        x = xp.stack(self.meshgrid(xp), axis=-1)
+        return x
