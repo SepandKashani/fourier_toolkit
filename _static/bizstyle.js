5e54cb41991c92be375b856e02cdf80b4c906d7c
Sepand KASHANI <sepand@kashani.ch>
2026-01-08 12:09:24 +0100

    HTML docs for v0.1.1

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Fourier Toolkit 0.1.dev107 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Fourier Toolkit 0.2.dev0 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev107',
+    VERSION: '0.2.dev0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
