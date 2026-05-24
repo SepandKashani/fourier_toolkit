fdaedd83c04425658cda66c6d51bb23f99198b20
Sepand KASHANI <sepand@kashani.ch>
2026-05-24 18:45:10 +0200

    typing: redefine builtin types to conform to Array API.
    We also drop the `array-api-typing` package.

--- src/fourier_toolkit/typing.py
+++ src/fourier_toolkit/typing.py
@@ -1,34 +1,114 @@
-# This module defines array types used throughout the codebase.
+# This module defines types used throughout the codebase.
 
-from types import ModuleType
-from typing import Union
+from __future__ import annotations
 
-import numpy as np
-from array_api_typing import Array
+import typing as typ
 
 __all__ = [
-    "ArrayR",
     "ArrayC",
+    "ArrayNameSpace",
+    "ArrayR",
     "ArrayRC",
+    "DType",
 ]
 
-ArrayR = Union[
-    Array[np.float32, ModuleType],
-    Array[np.float64, ModuleType],
+
+# Data Types ===================================================================
+class DType:
+    pass
+
+
+class int32(DType):
+    pass
+
+
+class int64(DType):
+    pass
+
+
+class float32(DType):
+    pass
+
+
+class float64(DType):
+    pass
+
+
+class complex64(DType):
+    pass
+
+
+class complex128(DType):
+    pass
+
+
+DTypeT = typ.TypeVar("DTypeT", bound=DType)
+"""
+Data type objects used as `dtype` specifiers in functions and methods.
+"""
+
+
+# Array NameSpace ==============================================================
+@typ.runtime_checkable
+class ArrayNameSpace(typ.Protocol):
+    """
+    Object that has all the array API functions on it.
+    """
+
+    # For typing purposes, we limit ourselves to a subset of the array API.
+
+    def __array_namespace_info__(self) -> typ.Any: ...
+
+    int32: type[int32]
+    int64: type[int64]
+    float32: type[float32]
+    float64: type[float64]
+    complex64: type[complex64]
+    complex128: type[complex128]
+
+
+@typ.runtime_checkable
+class Array(typ.Protocol[DTypeT]):
+    """
+    Array-API-compatible array.
+    """
+
+    dtype: DTypeT
+    device: typ.Any
+    ndim: int
+    shape: tuple[int, ...]
+    size: int
+
+    @property
+    def T(self) -> Array: ...
+
+    @property
+    def mT(self) -> Array: ...
+
+    def __array_namespace__(
+        self,
+        *,
+        api_version: str | None = None,
+    ) -> ArrayNameSpace: ...
+
+
+ArrayR = typ.Union[
+    Array[float32],
+    Array[float64],
 ]
 """
 Array-API-compatible array of dtype float[32,64].
 """
 
-ArrayC = Union[
-    Array[np.complex64, ModuleType],
-    Array[np.complex128, ModuleType],
+ArrayC = typ.Union[
+    Array[complex64],
+    Array[complex128],
 ]
 """
 Array-API-compatible array of dtype complex[64,128].
 """
 
-ArrayRC = Union[
+ArrayRC = typ.Union[
     ArrayR,
     ArrayC,
 ]
