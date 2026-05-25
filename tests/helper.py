2aa9c6b5433bcfdfceb07b2e1ea2423fb04733d2
Sepand KASHANI <sepand@kashani.ch>
2026-05-25 14:31:33 +0000

    [helper] Full array-API compliance.

--- tests/helper.py
+++ tests/helper.py
@@ -1,6 +1,6 @@
 import array_api_compat as aac
 import array_api_extra as aae
-
+import opt_einsum as oe
 import fourier_toolkit.typing as ftkt
 import fourier_toolkit.util as ftku
 
@@ -31,3 +31,115 @@ def allclose(a: ftkt.Array, b: ftkt.Array, dtype: ftkt.DType) -> bool:
 
     match = xp.all(aae.isclose(a, b, atol=atol, equal_nan=True))
     return bool(match)
+
+
+def rel_l2_distance(a: ftkt.Array, b: ftkt.Array, D: int) -> ftkt.Array:
+    r"""
+    Relative L2-distance between 2 vectors.
+
+    Parameters
+    ----------
+    a: Array[float,complex]
+        (..., N1,...,ND)
+    b: Array[float,complex]
+        (..., N1,...,ND)
+    D: int
+
+    Returns
+    -------
+    rel_dist: Array[float]
+        (...,)  \norm{a-b}{2} / \norm{b}{2}
+    """
+    assert D > 0
+    xp = aac.array_namespace(a)
+
+    axis = tuple(range(-D, 0))
+    num = xp.real(xp.sum((a - b) * xp.conj(a - b), axis=axis))
+    den = xp.real(xp.sum(b * xp.conj(b), axis=axis))
+    rel_dist = xp.sqrt(num / den)
+    return rel_dist
+
+
+def rel_linf_distance(a: ftkt.Array, b: ftkt.Array, D: int) -> ftkt.Array:
+    r"""
+    Relative Linf-distance between 2 vectors.
+
+    Parameters
+    ----------
+    a: Array[float,complex]
+        (..., N1,...,ND)
+    b: Array[float,complex]
+        (..., N1,...,ND)
+    D: int
+
+    Returns
+    -------
+    rel_dist: Array[float]
+        (...,)  \norm{a-b}{\inf} / \norm{b}{\inf}
+    """
+    assert D > 0
+    xp = aac.array_namespace(a)
+
+    axis = tuple(range(-D, 0))
+    num = xp.max(xp.abs(a - b), axis=axis)
+    den = xp.max(xp.abs(b), axis=axis)
+    rel_dist = num / den
+    return rel_dist
+
+
+def rel_l2_close(a: ftkt.Array, b: ftkt.Array, D: int, eps: float) -> bool:
+    r"""
+    Are `a` and `b` close up to a prescribed relative L2 error?
+
+    Parameters
+    ----------
+    a: Array[float,complex]
+        (..., N1,...,ND)
+    b: Array[float,complex]
+        (..., N1,...,ND)
+    D: int
+    eps: float
+        [0, 1] tolerance.
+
+    Returns
+    -------
+    close: bool
+        \norm{a - b}{2} <= eps * \norm{b}{2}
+    """
+    assert D > 0
+    assert 0 <= eps <= 1
+    xp = aac.array_namespace(a)
+
+    rel_dist = rel_l2_distance(a, b, D)
+    close = xp.all(rel_dist <= eps)
+    return bool(close)
+
+
+def inner_product(a: ftkt.Array, b: ftkt.Array, D: int) -> ftkt.Array:
+    r"""
+    Compute stack-wize inner-product.
+
+    Parameters
+    ----------
+    a: Array[float,complex]
+        (..., N1,...,ND)
+    b: Array[float,complex]
+        (..., N1,...,ND)
+    D: int
+
+    Returns
+    -------
+    c: Array[float,complex]
+        (...,) inner-products <a,b> = \sum_{k} a_{k} \conj{b_{k}}
+    """
+    xp = aac.array_namespace(a)
+
+    a_ind = b_ind = (Ellipsis, *range(D))
+    c_ind = (Ellipsis,)
+
+    c = oe.contract(
+        *(a, a_ind),
+        *(xp.conj(b), b_ind),
+        c_ind,
+    )
+    return c
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,7 +1,20 @@
 import numpy as np
 import pytest
 
+import fourier_toolkit.typing as ftkt
+
 from . import helper
+
+parametrize_stack = pytest.mark.parametrize(
+    "sh_stack",
+    [
+        (),
+        (1,),
+        (2, 3),
+        (2, 1),
+    ],
+)
+parametrize_dim = pytest.mark.parametrize("D", [1, 2, 3])
 
 
 class TestAllClose:
@@ -17,6 +30,7 @@ class TestAllClose:
         ],
     )
     def test_accepted_numerical_types(self, array_backend, ab_dtype):
+        # any `ab_dtype` should be comparable with allclose()
         rng = np.random.default_rng()
 
         sh = (5, 3, 4)
@@ -35,7 +49,8 @@ class TestAllClose:
 
     @pytest.mark.parametrize("input_dtype", ["float64", "complex128"])
     @pytest.mark.parametrize("precision", ["low", "high"])
-    def test_same_prec_equivalence(self, array_backend, input_dtype, precision):
+    def test_value(self, array_backend, input_dtype, precision):
+        # allclose()-ness depends on requested precision
         rng = np.random.default_rng()
 
         sh = (5, 3, 4)
@@ -68,17 +83,112 @@ class TestAllClose:
 
         assert helper.allclose(a, b, comparison_dtype)
 
-    def test_broadcasted_inputs(self, array_backend):
+
+class DistanceMixin:
+    @parametrize_stack
+    @parametrize_dim
+    def test_value(self, array_backend, sh_stack, D):
         rng = np.random.default_rng()
-        a = rng.standard_normal((1, 5, 3))
-        b = np.broadcast_to(a, (6, 5, 3)).copy()
+
+        # We test with (a=complex,b=real) to ensure mixed-dtype works
+        sh_core = tuple(rng.integers(2, 6, size=D))
+        a = rng.standard_normal(sh_stack + sh_core)
+        a = a + 1j * rng.standard_normal(sh_stack + sh_core)
+        b = rng.standard_normal(sh_stack + sh_core)
+        c_gt = self.compute_distance_gt(a, b, D)
 
         xp = array_backend.xp
         kwargs = dict(
-            dtype=xp.float64,
             device=array_backend.device,
         )
         a = xp.asarray(a, **kwargs)
         b = xp.asarray(b, **kwargs)
+        c_gt = xp.asarray(c_gt, **kwargs)
 
-        assert helper.allclose(a, b, a.dtype)
+        c = self.compute_distance(a, b, D)
+        assert c.shape == sh_stack
+        assert helper.allclose(c, c_gt, a.dtype)
+
+
+class TestRelL2Distance(DistanceMixin):
+    @classmethod
+    def compute_distance(cls, a: ftkt.Array, b: ftkt.Array, D: int) -> ftkt.Array:
+        return helper.rel_l2_distance(a, b, D)
+
+    @classmethod
+    def compute_distance_gt(cls, a: np.ndarray, b: np.ndarray, D: int) -> np.ndarray:
+        axis = tuple(range(-D, 0))
+        return np.sqrt(
+            np.sum((a - b) * (a - b).conj(), axis=axis).real
+            / np.sum(b * b.conj(), axis=axis).real
+        )
+
+
+class TestRelLInfDistance(DistanceMixin):
+    @classmethod
+    def compute_distance(cls, a: ftkt.Array, b: ftkt.Array, D: int) -> ftkt.Array:
+        return helper.rel_linf_distance(a, b, D)
+
+    @classmethod
+    def compute_distance_gt(cls, a: np.ndarray, b: np.ndarray, D: int) -> np.ndarray:
+        axis = tuple(range(-D, 0))
+        return np.max(np.abs(a - b), axis=axis) / np.max(np.abs(b), axis=axis)
+
+
+class TestRelL2Close:
+    @parametrize_stack
+    @parametrize_dim
+    def test_value(self, array_backend, sh_stack, D):
+        # we verify 2 special cases:
+        # (1) rel_l2_distance(a=0,b=random,eps=1) == True
+        # (2) rel_l2_distance(a=b,b=random,eps=0) == True
+        rng = np.random.default_rng()
+
+        sh_core = tuple(rng.integers(2, 6, size=D))
+        b = rng.standard_normal(sh_stack + sh_core)
+        b = b + 1j * rng.standard_normal(sh_stack + sh_core)
+        a1 = np.zeros(sh_stack + sh_core)
+        a2 = b.copy()
+
+        xp = array_backend.xp
+        kwargs = dict(
+            dtype=xp.complex128,
+            device=array_backend.device,
+        )
+        a1 = xp.asarray(a1, **kwargs)
+        a2 = xp.asarray(a2, **kwargs)
+        b = xp.asarray(b, **kwargs)
+
+        assert helper.rel_l2_close(a1, b, D, eps=1)
+        assert helper.rel_l2_close(a2, b, D, eps=0)
+
+
+class TestInnerProduct:
+    @parametrize_stack
+    @parametrize_dim
+    def test_value(self, array_backend, sh_stack, D):
+        rng = np.random.default_rng()
+
+        sh_core = tuple(rng.integers(2, 6, size=D))
+        a = rng.standard_normal(sh_stack + sh_core)
+        a = a + 1j * rng.standard_normal(sh_stack + sh_core)
+        b = rng.standard_normal(sh_stack + sh_core)
+        b = b + 1j * rng.standard_normal(sh_stack + sh_core)
+        c_gt = np.zeros(sh_stack, dtype=np.complex128)
+        for idx in np.ndindex(sh_stack):
+            c_gt[idx] = np.sum(a[idx] * b[idx].conj())
+
+        xp = array_backend.xp
+        kwargs = dict(
+            dtype=xp.complex128,
+            device=array_backend.device,
+        )
+        a = xp.asarray(a, **kwargs)
+        b = xp.asarray(b, **kwargs)
+        c_gt = xp.asarray(c_gt, **kwargs)
+
+        assert helper.allclose(
+            helper.inner_product(a, b, D),
+            c_gt,
+            xp.complex128,
+        )
