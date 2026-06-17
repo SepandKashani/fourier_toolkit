6702d7786a6aca0d7cb5c403a0ba9225029e7a56
Sepand KASHANI <sepand@kashani.ch>
2026-06-17 19:10:26 +0200

    [ufft][BugFix] CZT modulation vector mis-aligned.

--- src/fourier_toolkit/ufft.py
+++ src/fourier_toolkit/ufft.py
@@ -206,7 +206,7 @@ class _CZT:
             _Wk2 = W ** ((k**2) / 2)
             _AWk2 = (A ** -k[:N]) * _Wk2[:N]
             _FWk2 = xp.fft.fft(
-                xp.conj(xp.concat([xp.flip(_Wk2)[: (N - 1)], _Wk2[:M]])),
+                xp.conj(xp.concat([xp.flip(_Wk2[1:N]), _Wk2[:M]])),
                 n=L,
             )
             _Wk2 = _Wk2[:M]
